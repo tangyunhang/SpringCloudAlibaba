8005b9d889ceb6e9ba7fd58f4ddfd831e002f429
tangyunhang <18815292164@163.com>
2022-05-16 17:06:02 +0800

    SpringCloudAlibaba

--- day01-SpringCloudAlibaba/04-资料/01-consumer/src/main/java/com/abc/bean/Depart.java
+++
@@ -1,9 +0,0 @@
-package com.abc.bean;
-
-import lombok.Data;
-
-@Data//作用相当于：@Getter @Setter @RequiredArgsConstructor @ToString @EqualsAndHashCode
-public class Depart {
-    private Integer id;
-    private String name;
-}
--- day01-SpringCloudAlibaba/04-资料/01-provider/src/main/java/com/abc/repository/DepartRepository.java
+++
@@ -1,7 +0,0 @@
-package com.abc.repository;
-
-import com.abc.bean.Depart;
-import org.springframework.data.jpa.repository.JpaRepository;
-//持久层
-public interface DepartRepository extends JpaRepository<Depart, Integer> {
-}
